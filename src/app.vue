23c0741f678a09f3f85aaa5f42c1ba2e7269a64d
SilentDepth <silentdepth@outlook.com>
2020-05-13 12:44:48 +0800

    feat: add code hints

--- src/app.vue
+++ src/app.vue
@@ -9,6 +9,38 @@
       </p>
       <input v-model="input" type="text" class="block w-full p-5 font-mono">
     </div>
+    <div data-section="code-hints" class="self-center mt-10 flex">
+      <section class="flex-grow flex-shrink">
+        <h2>Text Color</h2>
+        <div class="grid grid-flow-col grid-rows-4 row-gap-3 col-gap-4">
+          <p><code>&0</code><span class="mc-0">black</span></p>
+          <p><code>&1</code><span class="mc-1">dark blue</span></p>
+          <p><code>&2</code><span class="mc-2">dark green</span></p>
+          <p><code>&3</code><span class="mc-3">dark aqua</span></p>
+          <p><code>&4</code><span class="mc-4">dark red</span></p>
+          <p><code>&5</code><span class="mc-5">dark purple</span></p>
+          <p><code>&6</code><span class="mc-6">gold</span></p>
+          <p><code>&7</code><span class="mc-7">gray</span></p>
+          <p><code>&8</code><span class="mc-8">dark gray</span></p>
+          <p><code>&9</code><span class="mc-9">blue</span></p>
+          <p><code>&a</code><span class="mc-a">green</span></p>
+          <p><code>&b</code><span class="mc-b">aqua</span></p>
+          <p><code>&c</code><span class="mc-c">red</span></p>
+          <p><code>&d</code><span class="mc-d">light purple</span></p>
+          <p><code>&e</code><span class="mc-e">yellow</span></p>
+          <p><code>&f</code><span class="mc-f">white</span></p>
+        </div>
+      </section>
+      <section class="ml-4 flex-grow flex-shrink">
+        <h2>Font Style</h2>
+        <div class="space-y-3">
+          <p><code>&l</code><span class="mc-l">bold</span></p>
+          <p><code>&m</code><span class="mc-m">strikethrough</span></p>
+          <p><code>&n</code><span class="mc-n">underline</span></p>
+          <p><code>&o</code><span class="mc-o">italic</span></p>
+        </div>
+      </section>
+    </div>
     <footer class="mt-auto py-5">
       <p class="text-gray-600 text-center">Built with ❤ by KPCC</p>
       <p class="mt-4 text-sm text-gray-400 text-center">Vue {{ version }}</p>
@@ -34,3 +66,19 @@
     },
   }
 </script>
+
+<style lang="scss" scoped>
+  [data-section=code-hints] {
+    h2 {
+      @apply mb-5 text-gray-600 font-medium tracking-wider uppercase;
+    }
+
+    p {
+      @apply flex items-center;
+    }
+
+    code {
+      @apply mr-2 p-1 bg-gray-200 rounded;
+    }
+  }
+</style>
